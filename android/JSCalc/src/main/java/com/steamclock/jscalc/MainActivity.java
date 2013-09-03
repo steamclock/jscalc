7321e9ee9e241b2e861fe5bb340855bebc3e359f
Chani Armitage <chani@steamclock.com>
2013-09-03 16:36:45 -0700

    Make button clicks call js

--- android/JSCalc/src/main/java/com/steamclock/jscalc/MainActivity.java
+++ android/JSCalc/src/main/java/com/steamclock/jscalc/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import junit.framework.Assert;
 
 import org.mozilla.javascript.*;
 
@@ -12,6 +16,7 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getName();
 
     public TextView display;
+    private Scriptable scope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,33 +35,66 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    public void buttonPressed (View view) {
+        final Button button = (Button) view;
+
+        runInJSContext(new JSRunnable() {
+            @Override
+            public void run(Context cx) {
+                Function testClick = (Function) scope.get("testClick", scope);
+                Assert.assertNotNull(testClick);
+
+                Object wButton = Context.javaToJS(button, scope);
+                Object[] args = {wButton};
+
+                Object result = testClick.call(cx, scope, scope, args);
+                Log.d(TAG, Context.toString(result));
+            }
+        });
+    }
+
     public void testJs() {
-        Context cx = Context.enter();
+        runInJSContext(new JSRunnable() {
+            @Override
+            public void run(Context cx) {
+                scope = cx.initStandardObjects();
+
+                //set up console.log
+                Object wConsole = Context.javaToJS(new ConsoleWrapper(), scope);
+                ScriptableObject.putProperty(scope, "console", wConsole);
+
+                //set up display widget
+                Object wDisplay = Context.javaToJS(display, scope);
+                ScriptableObject.putProperty(scope, "display", wDisplay);
+
+
+                String test = "console.log('o hai'); " +
+                        "var text = 'hello javascript';" +
+                        "display.setText(text);" +
+                        "function testClick(button) {" +
+                        " console.log(button.getText());" +
+                        "}";
+
+                Object result = cx.evaluateString(scope, test, "testJs", 1, null);
+                Log.d(TAG, Context.toString(result));
+            }
+        });
+    }
+
+    private void runInJSContext(JSRunnable code) {
+        final Context cx = Context.enter();
         cx.setOptimizationLevel(-1); //needed for the stock rhino jar. TODO: try the ASE jar instead.
         try {
-            Scriptable scope = cx.initStandardObjects();
-
-            //set up console.log
-            Object console = Context.javaToJS(new ConsoleWrapper(), scope);
-            ScriptableObject.putProperty(scope, "console", console);
-
-            //set up display widget
-            Object wDisplay = Context.javaToJS(display, scope);
-            ScriptableObject.putProperty(scope, "display", wDisplay);
-
-
-            String test = "console.log('o hai'); " +
-                    "var text = 'hello javascript';" +
-                    "display.setText(text);";
-
-            Object result = cx.evaluateString(scope, test, "testJs", 1, null);
-
-            Log.d(TAG, Context.toString(result));
+            code.run(cx);
         } catch (Exception ex) {
             Log.e(TAG, ex.toString());
         } finally {
             Context.exit();
         }
+    }
+
+    private interface JSRunnable {
+        public void run(Context cx);
     }
 
     public class ConsoleWrapper {
