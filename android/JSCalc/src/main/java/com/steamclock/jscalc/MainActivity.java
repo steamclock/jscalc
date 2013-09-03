4a3aae8003e35eea49e31983e51e17e69bc994d7
Chani Armitage <chani@steamclock.com>
2013-09-03 14:56:52 -0700

    Add rhino and get it running

--- android/JSCalc/src/main/java/com/steamclock/jscalc/MainActivity.java
+++ android/JSCalc/src/main/java/com/steamclock/jscalc/MainActivity.java
@@ -2,7 +2,10 @@ package com.steamclock.jscalc;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+
+import org.mozilla.javascript.*;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testJs();
     }
 
 
@@ -19,5 +23,22 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    public void testJs() {
+        Context cx = Context.enter();
+        cx.setOptimizationLevel(-1); //needed for the stock rhino jar. TODO: try the ASE jar instead.
+        try {
+            Scriptable scope = cx.initStandardObjects();
+
+            String test = "['hello', 'world'].join(' ');";
+
+            Object result = cx.evaluateString(scope, test, "<cmd>", 1, null);
+
+            Log.d("tag", Context.toString(result));
+        } catch (Exception ex) {
+            Log.e("tag", ex.toString());
+        } finally {
+            Context.exit();
+        }
+    }
 }
