b9f88e1d4e9d9ba7c00ca2a2e0883a615e9216b9
Chani Armitage <chani@steamclock.com>
2013-09-04 11:41:27 -0700

    Don't allow invalid button presses. Fixes #1, fixes #2

--- Common/calc.js
+++ Common/calc.js
@@ -1,6 +1,8 @@
 // default state of calculator object
 var initialState = {
     currentExpression: "0",
+    endsInSymbol: false,
+    inDecimal: false,
     fragile: true //fragile means it was just eval'd, and we may want to reuse it or wipe it depending on the next keypress.
 };
 
@@ -12,33 +14,54 @@ if(typeof calculator != 'undefined') {
 // define the calculator object
 calculator = {
     currentExpression: initialState.currentExpression,
-    
+    endsInSymbol: initialState.endsInSymbol,
+    inDecimal: initialState.inDecimal,
     fragile: initialState.fragile,
     
     buttonPress: function (operation) {
         var isNumber = !isNaN(operation);
         
         if(operation === "=") {
+            if (this.endsInSymbol) return;
+
             this.currentExpression = eval(this.currentExpression).toString();
             this.fragile = true;
-        }
-        else if(!isNumber) {
+        } else if (operation === '.') {
+            if(this.fragile) {
+                this.clear();
+            }
+
+            if (this.inDecimal) return;
+
+            this.inDecimal = true;
+            this.currentExpression += operation;
+            this.endsInSymbol = true;
+        } else if(!isNumber) {
+            if (this.endsInSymbol && (operation === '*' || this.inDecimal)) return;
+
             this.currentExpression = this.currentExpression + " " + operation + " ";
             this.fragile = false;
-        }
-        else {
+            this.endsInSymbol = true;
+            this.inDecimal = false;
+        } else { //number
             if(this.fragile) {
-                this.currentExpression = "";
-                this.fragile = false;
+                this.clear();
             }
             
             this.currentExpression = this.currentExpression + operation;
+            this.endsInSymbol = false;
         }
         
         display.setText(this.currentExpression);
         
+    },
+    clear: function(){
+        this.currentExpression = "";
+        this.fragile = false;
+        this.inDecimal = false;
+        this.endsInSymbol = false;
     }
-}
+};
 
 display.setText(calculator.currentExpression);
 
